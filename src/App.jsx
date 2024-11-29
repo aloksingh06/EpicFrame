9dc8d170fdcef2a3c2e5577998c9a055e9d49646
aloksingh06 <aloksingh6761@gmail.com>
2024-11-29 16:13:06 +0530

    correction in rounter

--- src/App.jsx
+++ src/App.jsx
@@ -2,39 +2,45 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./Components/Home";
 import Trending from "./Components/Trending";
-import Popural from "./Components/Popural";
-import Movies from "./Components/Movies";
+import Popular from "./Components/Popural";
+import Movies from "./Components/Movies"; // Movies page, e.g., list of movies
 import Tv from "./Components/Tv";
 import People from "./Components/People";
-import MovieDetails from "./Components/MovieDetails";
+import MovieDetails from "./Components/MovieDetails"; // Specific movie details
 import TvDetails from "./Components/TvDetails";
 import PersonDetails from "./Components/PersonDetails";
 import Trailer from "./Components/partial/Trailer";
-import Notfound from "./Components/partial/Notfound";
+import NotFound from "./Components/partial/Notfound";
 import About from "./Components/About";
 
 function App() {
   return (
-    <div className=" bg-[#1f1E24] w-full h-full flex">
+    <div className="bg-[#1f1E24] w-full h-full flex">
       <Routes>
+        {/* Main Routes */}
         <Route path="/" element={<Home />} />
         <Route path="/trending" element={<Trending />} />
-        <Route path="/popular" element={<Popural />} />
-        <Route path="/movies" element={<Movies />} />
+        <Route path="/popular" element={<Popular />} />
+        <Route path="/movies" element={<Movies />} /> {/* Movies List */}
         <Route path="/about" element={<About />} />
-        <Route path="/movies/details/:id" element={<MovieDetails />}>
-          <Route path="/movies/details/:id/trailer" element={<Trailer />} />
-        </Route>
+
+        {/* Movies (specific movie) Details with Nested Trailer */}
         <Route path="/movie/details/:id" element={<MovieDetails />}>
-          <Route path="/movie/details/:id/trailer" element={<Trailer />} />
+          <Route path="trailer" element={<Trailer />} />
         </Route>
+
+        {/* TV Routes with Nested Trailer */}
         <Route path="/tv" element={<Tv />} />
-        <Route path="/tv/details/:id" element={<TvDetails />} >
-        <Route path="/tv/details/:id/trailer" element={<Trailer />} />
+        <Route path="/tv/details/:id" element={<TvDetails />}>
+          <Route path="trailer" element={<Trailer />} />
         </Route>
+
+        {/* People Routes */}
         <Route path="/people" element={<People />} />
         <Route path="/people/details/:id" element={<PersonDetails />} />
-        <Route path="*" element={<Notfound />} />
+
+        {/* Catch-All Route */}
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
